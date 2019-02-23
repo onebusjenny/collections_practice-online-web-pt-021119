88bc9c0a0cfb55852954a50474616affde7c202b
Jenny Chu <jchudesign0930@gmail.com>
2019-02-23 21:57:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc (array)
+  array.sort
+end
+
+
+def sort_array_desc (array)
+
+  
+end
+
+
+def sort_array_char_count
+  array.length.sort
+end
+
+
+def swap_elements(array)
+  array.sort do |a,b|
+    a<=>b
+end
+
+
+
+def reverse_array(array)
+  array.reverse
+end
+
+
+
+def kesha_maker (array)
+  array.each do |num|
+    
+end
+
+
+def find_a
+  
+end
+
+
+
+def sum_array
+  
+end
+
+
+
+def add_s
+  
+end
+
+
+
 
